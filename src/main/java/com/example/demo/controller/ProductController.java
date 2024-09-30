9dae120d52c18a2ca4c8e19d1e636ed07b04d88f
Kourosh Talebzadeh <Kourosh.Talebzadeh@iths.se>
2024-09-30 19:19:36 +0200

    Add endpoint getAllProducts

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -1,7 +1,27 @@
 package com.example.demo.controller;
 
+import com.example.demo.entity.Product;
+import com.example.demo.service.ProductService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/product")
 public class ProductController {
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/allproducts")
+    public ResponseEntity<Iterable<Product>> getAllProducts(){
+        Iterable<Product> products= productService.getAllProducts();
+        return ResponseEntity.ok(products);
+    }
+
 }
--- src/main/java/com/example/demo/service/ProductService.java
+++ src/main/java/com/example/demo/service/ProductService.java
@@ -1,7 +1,20 @@
 package com.example.demo.service;
 
+import com.example.demo.entity.Product;
+import com.example.demo.repository.ProductRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductService {
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public Iterable<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
 }
