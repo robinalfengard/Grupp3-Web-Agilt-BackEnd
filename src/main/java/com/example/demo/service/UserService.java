d3f2c6b1e18319ce3fbb96ee5d6d0b753e44d37f
Kourosh Talebzadeh <Kourosh.Talebzadeh@iths.se>
2024-09-30 18:49:08 +0200

    Create Service

---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,18 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void save(User usersignup) {
+        userRepository.save(usersignup);
+    }
+}
