9e9599ff0ccca465231689eaae1ad6d2259d7dc5
Robin Alfengård <robin.alfengard@hotmail.com>
2024-09-30 14:44:36 +0200

    init commit

---
+++ src/main/java/com/example/demo/Grupp3ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupp3ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupp3ProjectApplication.class, args);
+	}
+
+}
