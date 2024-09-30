2a2831e2321aac19b89fad264a8075c43aa228cc
Kourosh Talebzadeh <Kourosh.Talebzadeh@iths.se>
2024-09-30 18:49:29 +0200

    Create Controller

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.User;
+import com.example.demo.service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/signup")
+    public void signup(@RequestBody User usersignup) {
+        userService.save(usersignup);
+    }
+}
