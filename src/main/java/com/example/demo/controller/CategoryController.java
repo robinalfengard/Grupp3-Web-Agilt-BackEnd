4b31026e422a5dc4259af87f54d10817a28dbb89
Kourosh Talebzadeh <Kourosh.Talebzadeh@iths.se>
2024-10-07 13:04:32 +0200

    Add @CrossOrigin(origins = "http://localhost:3000") in CategoryController

--- src/main/java/com/example/demo/controller/CategoryController.java
+++ src/main/java/com/example/demo/controller/CategoryController.java
@@ -3,12 +3,14 @@ package com.example.demo.controller;
 import com.example.demo.entity.Category;
 import com.example.demo.service.CategoryService;
 import com.example.demo.service.ProductService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/category")
+@CrossOrigin(origins = "http://localhost:3000")
 public class CategoryController {
     private final CategoryService categoryService;
     private final ProductService productService;
