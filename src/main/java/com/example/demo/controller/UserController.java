77b092cc7aa614797e94f7bd17a1ce4fbc8fe061
Kourosh Talebzadeh <Kourosh.Talebzadeh@iths.se>
2024-10-12 21:57:45 +0200

    Change port from 8081 to 3000 in Controller

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
-@CrossOrigin(origins = "http://localhost:8081")
+@CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
     private final UserService userService;
 
