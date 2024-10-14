60defe5f057790aa1f736036e072fc89a73adf09
Kourosh Talebzadeh <Kourosh.Talebzadeh@iths.se>
2024-10-14 14:40:29 +0200

    Add deleteProductFromCart method in soldItemService

--- src/main/java/com/example/demo/repository/SoldItemRepository.java
+++ src/main/java/com/example/demo/repository/SoldItemRepository.java
@@ -3,8 +3,12 @@ package com.example.demo.repository;
 import com.example.demo.entity.Product;
 import com.example.demo.entity.SoldProduct;
 import com.example.demo.entity.User;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -12,4 +16,9 @@ import java.util.List;
 public interface SoldItemRepository extends CrudRepository<SoldProduct, Long> {
 
     List<SoldProduct> findProductsByUser(User user);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM SoldProduct sp WHERE sp.user = :user AND sp.product = :product")
+    void deleteByUserAndProduct(@Param("user") User user, @Param("product") Product product);
 }
