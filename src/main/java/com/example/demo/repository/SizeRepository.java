c816d3d8328c04aa3323a7592a6106c595e10fb8
maxerkmar <max.erkmar@iths.se>
2024-10-21 14:07:38 +0200

    changes in size repo to get default sizes

--- src/main/java/com/example/demo/repository/SizeRepository.java
+++ src/main/java/com/example/demo/repository/SizeRepository.java
@@ -1,9 +1,15 @@
 package com.example.demo.repository;
 import com.example.demo.entity.Size;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SizeRepository extends CrudRepository<Size, Long> {
+
+    @Query("SELECT s FROM Size s WHERE s.name IN ('S', 'M', 'L', 'XL')")
+    List<Size> getDefaultSizes();
 
 }
