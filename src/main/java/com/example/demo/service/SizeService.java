67a369cf9ae44ea3f6ae3857170b3b07800d7ae6
maxerkmar <max.erkmar@iths.se>
2024-10-21 14:15:38 +0200

    updated service to get default sizes from repo

--- src/main/java/com/example/demo/service/SizeService.java
+++ src/main/java/com/example/demo/service/SizeService.java
@@ -1,7 +1,21 @@
 package com.example.demo.service;
 
+import com.example.demo.entity.Size;
+import com.example.demo.repository.SizeRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class SizeService {
+
+    private final SizeRepository sizeRepository;
+
+    public SizeService(SizeRepository sizeRepository) {
+        this.sizeRepository = sizeRepository;
+    }
+
+    public List<Size> getDefaultSizes() {
+        return sizeRepository.getDefaultSizes();
+    }
 }
